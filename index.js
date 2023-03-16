8bad4c85a1dfb51d8412dc89d9e87d475013eedd
Megan Dacus <meganjustice0609@gmail.com>
2023-03-16 11:57:16 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(function (element) {
+        return element.toLowerCase() === string.toLowerCase()
+    } )
+    
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(element) {
+        return element.startsWith(string)
+    } )
+}
+
+function matchName(array, string) {
+    return array.filter(function(object) {
+        return object.name === string
+    })
+}
